8cbb77374b97db5cfe6612e86f402148622e44e6
SkoobyDoo <theskoobydoo@gmail.com>
2018-10-15 20:24:11 -0700

    Initial commit. Item tooltips print self to logfile when holding control.

---
+++ init.lua
@@ -0,0 +1,25 @@
+long_name = "SkooBackpack"
+short_name = "skoobackpack"
+for_module = "tome"
+version = {1,5,10}
+addon_version = {0,0,1}
+weight = 1000
+author = { "SkoobyDoo (theskoobydoo@gmail.com)" }
+description = [[This addon assists you in optimizing your equipped gear by helping you evaluate the usefulness of items as well as automatically equip the best items available to you for each slot.
+
+Hotkey defaults are as follows and can be configured in the options screen:
+* A button - Does something
+
+Changes in 0.0.1:
+* Initial Release
+
+Upcoming Features (in no particular order):
+* Implementation.
+
+Github: https://github.com/SkoobyDoo/SkooBackpack
+]]
+tags = { 'skoobot','bot','ai' }
+--hooks = true
+--overload = true
+superload = true
+--data = true
