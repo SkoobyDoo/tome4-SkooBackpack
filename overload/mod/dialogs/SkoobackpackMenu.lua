7b7b13de2dbf897f86ebd9bbb0d64c39c67f58d7
SkoobyDoo <theskoobydoo@gmail.com>
2019-03-08 14:04:48 -0800

    Implement auto-equip functionality to evaluate/equip best scoring item for each slot. Resolves #5

--- overload/mod/dialogs/SkoobackpackMenu.lua
+++ overload/mod/dialogs/SkoobackpackMenu.lua
@@ -27,6 +27,46 @@ function _M:on_register()
 end
 
 local menuActions = {
+	equipitems = function()
+		print("[Skoobackpack] [Menu] equipitems menu action chosen.")
+		
+		-- map to store items by slot as we iterate through the inventory.
+		local itembuckets = {}
+		-- iterate through items in player's base inventory
+		for i,item in ipairs(game.player.inven[game.player.INVEN_INVEN]) do
+			if item.slot and game.player:canWearObject(item) then
+				if not itembuckets[item.slot] then
+					itembuckets[item.slot] = {}
+				end
+				itembuckets[item.slot][#itembuckets[item.slot]+1] = {item=item, score=item:evaluatePowerLevel(game.player)}
+				
+				print("[Skoobackpack] player can wear",item.name,"in slot",item.slot)
+			end
+		end
+		-- helper function to find player.INVEN_INVEN index 
+		local function findSlotId(toFind)
+			local inv = game.player.inven[game.player.INVEN_INVEN]
+			for i,item in ipairs(inv) do
+				if item == toFind then return i end
+			end
+			return nil
+		end
+		for slot,bucket in pairs(itembuckets) do
+			table.sort(bucket, function(a,b) return a.score>b.score end)
+			local thisslotid = game.player.inven_def[slot].id
+			if game.player.inven[thisslotid] then
+				local equippeditem = game.player.inven[thisslotid][1]
+				if not equippeditem or bucket[1].score > equippeditem:evaluatePowerLevel(game.player) then
+					print("[Skoobackpack] equipping item",bucket[1].item.name,
+						"score:",bucket[1].score,"in slot",slot, "old score:",equippeditem and equippeditem:evaluatePowerLevel(game.player) or "none")
+					local worn = game.player:doWear(game.player.INVEN_INVEN,findSlotId(bucket[1].item),bucket[1].item, game.player)
+				else
+					print("[Skoobackpack] item not good enough to equip",bucket[1].item.name,
+						"score:",bucket[1].score,"in slot",slot, "old score:",equippeditem and equippeditem:evaluatePowerLevel(game.player) or "none")
+				end
+			end
+		end
+	end,
 	statvalueconfig = function()
 		print("[Skoobackpack] [Menu] statvalueconfig menu action chosen.")
 		local d = require("mod.dialogs.ItemStatValueDialog").new(game.player)
@@ -45,7 +85,8 @@ end
 
 function _M:generateList()
 	local list = {
-		{1,name="Set Stat Values",order="statvalueconfig"},
+		{1,name="Autoequip Best Items",order="equipitems"},
+		{2,name="Set Stat Values",order="statvalueconfig"},
 		{999,name="Cancel",order="donothing"}
 	}
 
